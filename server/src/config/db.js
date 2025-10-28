1e53511eb5ef780a932773cbc16852e651961075
IJ Roy <141866152+ij-roy@users.noreply.github.com>
2025-10-28 11:19:16 +0530

    certificate

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,22 +1,21 @@
-// src/config/db.js
 import pkg from "pg";
+import fs from "fs";
+import path from "path";
 import "dotenv/config";
-
 const { Pool } = pkg;
+
+const ca = fs.readFileSync(path.resolve("certs/ca-certificate.crt")).toString();
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    require: true,                // ✅ explicitly require SSL
-    rejectUnauthorized: false,    // ✅ ignore DO’s self-signed cert
-  },
+  ssl: { ca }
 });
 
 export async function testConnection() {
   try {
-    const result = await pool.query("SELECT NOW()");
-    console.log("✅ Connected to Postgres:", result.rows[0].now);
-  } catch (err) {
-    console.error("❌ Database connection failed:", err.message);
+    const { rows } = await pool.query("SELECT NOW()");
+    console.log("✅ Connected to Postgres:", rows[0].now);
+  } catch (e) {
+    console.error("❌ Database connection failed:", e.message);
   }
 }
